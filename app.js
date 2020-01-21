2d4c864cbc773139819447fe609a6cc19b269d65
Deluxateer <deluxateer@gmail.com>
2020-01-21 11:06:10 -0600

    add connection to MongoDB Atlas DB; pull posts from DB

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 const _ = require('lodash');
 
-let posts = [];
+const { mongoURL } = require('./secret');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,8 +19,32 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const postSchema = {
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  }
+};
+
+const Post = mongoose.model('Post', postSchema);
+
 app.get('/', function(req, res) {
-  res.render('home', { homeStartingContent, posts });
+  Post.find({}, function(err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('home', { homeStartingContent, posts });
+    }
+  });
 });
 
 app.get('/about', function(req, res) {
@@ -37,22 +62,15 @@ app.get('/compose', function(req, res) {
 app.post('/compose', function(req, res) {
   const { postTitle, postBody } = req.body;
 
-  const post = {
-    postTitle,
-    postBody, 
-  };
+  const post = new Post({
+    title: postTitle,
+    content: postBody,
+  });
 
-  posts.push(post);
+  post.save();
 
   res.redirect('/');
 });
-
-
-
-
-
-
-
 
 app.get('/posts/:postName', function(req,res) {
   posts.forEach(function(post) {
